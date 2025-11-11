b073b48006c89172d1ab60b6e635967c921d8eae
anilkumarsripadam <127820058+anilkumarsripadam@users.noreply.github.com>
2025-11-11 14:11:27 +0530

    Create backend.tf
    tf state secure

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-anil-2025"
+    bucket         = "terraform-state-anil"
     key            = "env/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
+
